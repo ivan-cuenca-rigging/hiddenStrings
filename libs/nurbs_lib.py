965f3b99d8a5099d2eafd830bf8fe89b4ca59f8e
ivan-cuenca-rigging <ivan.cuenca.ruiz@gmail.com>
2023-07-15 20:21:56 +0200

    Fix: create nurbs

--- libs/nurbs_lib.py
+++ libs/nurbs_lib.py
@@ -14,17 +14,17 @@ def create_nurbs_from_points(descriptor,
     for point in points:
         positions.append(cmds.xform(point, query=True, worldSpace=True, translation=True))
 
-    crv_0 = cmds.curve(p=positions, degree=1)
-    crv_1 = cmds.curve(p=positions, degree=1)
+    crv_0 = cmds.curve(p=positions, degree=3)
+    crv_1 = cmds.curve(p=positions, degree=3)
     cmds.setAttr('{}.translate{}'.format(crv_0, axis.upper()), -width/2)
     cmds.setAttr('{}.translate{}'.format(crv_1, axis.upper()), width/2)
 
     nurbs_transform = cmds.loft(crv_0, crv_1, degree=3, constructionHistory=False)
     nurbs_transform = cmds.rename(nurbs_transform, '{}_{}_{}'.format(descriptor, side, usage_lib.nurbs))
 
-    cmds.rebuildSurface(nurbs_transform, constructionHistory=False, replaceOriginal=True, rebuildType=0,
-                        keepRange=False, keepControlPoints=False, direction=2,
-                        spansU=1, degreeU=1, spansV=int(len(points)), degreeV=3)
+    # cmds.rebuildSurface(nurbs_transform, constructionHistory=False, replaceOriginal=True, rebuildType=0,
+    #                     keepRange=False, keepControlPoints=False, direction=2,
+    #                     spansU=1, degreeU=1, spansV=int(len(points)), degreeV=3)
 
     cmds.delete(crv_0, crv_1)
 
