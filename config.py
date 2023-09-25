16fe2419fb1775d9a2cba49a036ec32f370678b4
ivan-cuenca-rigging <ivan.cuenca.ruiz@gmail.com>
2023-09-25 18:46:21 +0200

    update markingMenu click input

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 create_userSetup_bool = True
 
 load_markingMenu_bool = True
-markingMenu_click_input = 3  # 1 -> left click, 2 -> middle click, 3 -> right click
+markingMenu_click_input = 1  # 1 -> left click, 2 -> middle click, 3 -> right click
 
 load_hotkeys_bool = True
 
